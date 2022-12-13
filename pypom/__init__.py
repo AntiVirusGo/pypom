f74cb1f0e209002e4c432990ad64d7467bc8f08d
shenyanjian <shenyanjian@blingsec.cn>
2022-12-13 17:49:37 +0800

    init commit

---
+++ pypom/__init__.py
@@ -0,0 +1,7 @@
+
+
+from .pomparse import pomparser
+
+__all__ = []
+__version__ = "0.0.1"
+__doc__ = "使用时，如果对字段有疑问，可以直接打印结果看字段有什么"
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name='pypom',
+        version='0.1',
+        description='A third party Package to analyse pom data',
+        author='SenRanja',
+        author_email='2409879947@qq.com',
+        packages=['pypom']
+)
